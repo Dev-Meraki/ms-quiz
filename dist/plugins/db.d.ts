856dac90c371b8cf26b93c9bc8868c98a5c44559
Himanshu Saraswat <himanshusaraswat@outlook.com>
2023-12-10 18:35:39 +0530

    Check deploy

--- dist/plugins/db.d.ts
+++
@@ -1,3 +0,0 @@
-import { FastifyMongodbOptions } from '@fastify/mongodb';
-declare const _default: import("fastify").FastifyPluginCallback<FastifyMongodbOptions, import("fastify").RawServerDefault, import("fastify").FastifyTypeProviderDefault, import("fastify").FastifyBaseLogger>;
-export default _default;
